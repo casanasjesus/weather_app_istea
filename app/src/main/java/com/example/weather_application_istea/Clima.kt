968edc957159f4737e8966a0174a32342664a32a
Jesús Casañas <jcasanas@mobilecomputing.com.ar>
2025-05-21 16:42:31 -0300

    feat(models): Clima model created

---
+++ app/src/main/java/com/example/weather_application_istea/Clima.kt
@@ -0,0 +1,6 @@
+package com.example.weather_application_istea
+
+data class Clima (
+    val nombre: String
+    // Definir propiedades del objeto Clima
+)
--- app/src/main/java/com/example/weather_application_istea/clima/ClimaEstado.kt
+++ app/src/main/java/com/example/weather_application_istea/clima/ClimaEstado.kt
@@ -1,8 +1,10 @@
 package com.example.weather_application_istea.clima
 
+import com.example.weather_application_istea.Clima
+
 sealed class ClimaEstado {
     data object Vacio: ClimaEstado()
     data object Cargando: ClimaEstado()
     data class Error(val error:String): ClimaEstado()
-    data class Resultado(val clima: List<Any>): ClimaEstado()
+    data class Resultado(val clima: Clima): ClimaEstado()
 }
