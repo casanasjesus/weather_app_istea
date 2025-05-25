d69b1a89f2d062de4e3500c9ee551dac49b09549
Ian <agustinsitas38@gmail.com>
2025-05-25 01:07:30 -0300

    modelos: actualizar data class Ciudad con nombre, latitud, longitud y country

--- app/src/main/java/com/example/weather_application_istea/models/Ciudad.kt
+++ app/src/main/java/com/example/weather_application_istea/models/Ciudad.kt
@@ -1,9 +1,12 @@
 package com.example.weather_application_istea.models
 
-data class Ciudad (
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Ciudad(
     val name: String,
     val lat: Float,
     val lon: Float,
     val country: String,
-    val state: String = ""
+    val state: String? = null // Puede ser nulo según la API
 )
