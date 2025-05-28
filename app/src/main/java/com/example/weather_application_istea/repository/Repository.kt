cf8327951a2db67556d09516a82fa715dfafea08
Ian <agustinsitas38@gmail.com>
2025-05-28 20:13:03 -0300

    ➕ Agrega método getClimaActual a la interfaz Repository

--- app/src/main/java/com/example/weather_application_istea/repository/Repository.kt
+++ app/src/main/java/com/example/weather_application_istea/repository/Repository.kt
@@ -1,7 +1,9 @@
 package com.example.weather_application_istea.repository
 
 import com.example.weather_application_istea.models.Ciudad
+import com.example.weather_application_istea.models.ClimaActual
 
 interface Repository {
     suspend fun buscarCiudad(ciudad: String): List<Ciudad>
+    suspend fun getClimaActual(lat: Float, lon: Float): ClimaActual
 }
